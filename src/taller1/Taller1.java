a9bd4ba67b66eb6d76d868d5e51900bfe98c1e55
estudiante <estudiante@Lab408.pssc.utn.ac.cr>
2017-08-03 08:28:32 -0600

    Primer commit

---
+++ src/taller1/Taller1.java
@@ -0,0 +1,15 @@
+package taller1;
+
+public class Taller1 {
+    
+static void main(String[] args) {
+    int a = 0;
+        int b = 0;
+        int [] [] nom = new int [3] [3];
+        while (a<3){
+            System.out.println("Ingrese el primer nombre.");
+            
+        }
+    }
+    
+}
